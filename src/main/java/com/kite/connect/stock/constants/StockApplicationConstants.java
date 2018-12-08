3bf35e66811b1f31e727d3275a890c686d81309b
Saurabh Agrawal <rk.saurabh01@gmail.com>
2018-12-08 15:51:13 +0530

    First Commit

---
+++ src/main/java/com/kite/connect/stock/constants/StockApplicationConstants.java
@@ -0,0 +1,9 @@
+package com.kite.connect.stock.constants;
+
+/**
+ * Created by saurabhagrawal on 08/12/18.
+ */
+public class StockApplicationConstants {
+
+
+}
---
+++ src/main/java/com/kite/connect/stock/controllers/StockController.java
@@ -0,0 +1,19 @@
+package com.kite.connect.stock.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by saurabhagrawal on 08/12/18.
+ */
+@RestController
+public class StockController {
+    @RequestMapping(value = {"/test"}, method = RequestMethod.GET)
+    public ResponseEntity<?> test() {
+        return new ResponseEntity<String>("running", HttpStatus.OK);
+    }
+
+}
